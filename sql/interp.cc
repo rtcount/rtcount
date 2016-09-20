a4b182ab94c1699ed74d87133dfb06998ebdd1e4
Dongdong Deng <libfetion@gmail.com>
2016-09-20 13:27:57 +0000

    sql: remove ddd.c
    Signed-off-by: Dongdong Deng <libfetion@gmail.com>

--- sql/interp.cc
+++ sql/interp.cc
@@ -786,3 +786,44 @@ char tstr2[] = "select ddd from ddd.ddd with ddd and ddd where ddd >= 1466252795
 }
 
 */
+
+#ifdef __cplusplus    //__cplusplus是cpp中自定义的一个宏
+extern "C" {          //告诉编译器，这部分代码按C语言的格式进行编译，而不是C++的
+#endif
+const char*  RBparse(const char * str);
+
+const char* ddd(const char * str);
+
+#ifdef __cplusplus
+}
+#endif
+
+char  dd_xml_parser[1024*10];
+//int ddd(const char* str)
+const char* ddd(const char * str)
+{
+	char * dd = (char*)RBparse(str);
+	if (dd==NULL)
+		return "";
+
+	memset(dd_xml_parser, 0, 1024*10);
+	memcpy(dd_xml_parser, dd, strlen(dd));
+	free(dd);
+	return (const char*)dd_xml_parser;
+/*
+	   char tstr[] = "select asdddd from T_devices.ddd with zxc and xxx where created_at >= 1466252795 and product_id = 'T_devices.product_id' and product_id = \"T_devices.product_id\" and time =\"ad\" and time >'asd';";
+
+	   RBparse(tstr);
+
+	   char tstr2[] = "select ddd from ddd.ddd with ddd and ddd where ddd >= 1466252795 and ddd_id = 'ddd' and ddd = \"ddd\" and ddd =\"ddd\" and ddd >'ddd';";
+
+	   RBparse(tstr2);
+*/
+}
+
+/*
+int main()
+{
+return ddd();
+}
+*/
