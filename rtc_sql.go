9e28e98f2a97a7651829a42b36c30b0d7e1deea8
Dongdong Deng <libfetion@gmail.com>
2016-09-23 06:43:25 +0000

    merge rtc_sql_xml/query to rtc_sql
    Signed-off-by: Dongdong Deng <libfetion@gmail.com>

--- rtc_sql.go
+++ rtc_sql.go
@@ -8,22 +8,60 @@ import (
 	*/
 	"C"
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
-	"os"
-	"path/filepath"
 	"strconv"
 	"strings"
 	"time"
 	"unsafe"
 )
 
-func getCurrentDirectory() string {
-	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	if err != nil {
-		return ""
-	}
-	return strings.Replace(dir, "\\", "/", -1)
+type RTC_Sql struct {
+	XMLName xml.Name    `xml:"all"`
+	Op      string      `xml:"op"`
+	Table   string      `xml:"table"`
+	Key     string      `xml:"key"`
+	With    []string    `xml:"with"`
+	Condis  []condition `xml:"condition"`
 }
+
+type condition struct {
+	LhsAttr     string `xml:"lhsAttr"`
+	Op          string `xml:"op"`
+	Value       string `xml:"value"`
+	Val_type    string `xml:"val_type"`
+	i_columnref int
+}
+
+func RTC_sql_check(xmls string) (sql RTC_Sql, msg string, ret bool) {
+
+	bxml := []byte(xmls)
+
+	err := xml.Unmarshal(bxml, &sql)
+	if err != nil {
+		return sql, "xml parse<" + xmls + ">fail!", false
+	}
+
+	sql.Op = strings.ToLower(sql.Op)
+	sql.Table = strings.ToLower(sql.Table)
+	sql.Key = strings.ToLower(sql.Key)
+	sql.With[0] = strings.ToLower(sql.With[0])
+	sql.With[1] = strings.ToLower(sql.With[1])
+
+	for _, item := range sql.Condis {
+
+		if item.Val_type == "Attr" {
+			return sql, item.LhsAttr + item.Op + item.Value + " format error", false
+		}
+		item.LhsAttr = strings.ToLower(item.LhsAttr)
+	}
+
+	if 0 == 1 {
+		fmt.Println(sql)
+	}
+	return sql, "OK", true
+}
+
 func checkOP(OP string, t_key *Table_Key) (bool, string) {
 
 	i := GetIndexInArrayByString(OP, KEYOPS)
